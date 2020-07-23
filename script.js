7be8125afba755eecaf78467d9f0ae3cce298b12
Arman Aliani <armanaliani@gmail.com>
2020-07-23 18:21:22 -0400

    inital commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// namespace
+const catOrDog = {};
+
+// give values to answers
+catOrDog.questions = {
+    firstQuestion: {
+        firstAnswerValue: 3,
+        secondAnswerValue: 1,
+        thirdAnswerValue: -1,
+        fourthAnswerValue: -3,
+    },
+    secondQuestion: {
+        firstAnswerValue: 3,
+        secondAnswerValue: 1, 
+        thirdAnswerValue: -1, 
+        fourthAnswerValue: -3, 
+    },
+    thirdQuestion: {
+        firstAnswerValue: 3, 
+        secondAnswerValue: 1, 
+        thirdAnswerValue: -1,
+        fourthAnswerValue: -3,
+    }
+}
+
+console.log(catOrDog.questions);
+
+// get user input
+
+// calculate users score
+
+// displat user result based on score
